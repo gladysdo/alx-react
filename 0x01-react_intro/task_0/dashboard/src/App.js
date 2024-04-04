d7d50fd47b11bf8515d0088412f0b234a15b78c2
gladysdo <do.glad@gmail.com>
2024-04-04 09:19:50 -1200

    Basic application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import logo from './holberton logo.jpg';
 import { getFullYear, getFooterCopy } from './utils';
-=======
-import logo from './logo.svg';
->>>>>>> 2d95adf54d5195c5f648ee00c47c34e03155311a
 import './App.css';
 
 function App() {
@@ -12,33 +8,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
         <h1>School dashboard</h1>
         <hr className="horizontal-line" />
       </header>
 
-        <div className="App-body">
-          <p>Login to access the full dashboard</p>
-        </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
 
       <footer className="App-footer">
          <hr className="horizontal-line" />
           <p>Copyright 2020 - holberton School</p>
       </footer>
-=======
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 2d95adf54d5195c5f648ee00c47c34e03155311a
     </div>
   );
 }
--- 0x01-react_intro/task_0/dashboard/src/index.js
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 import Notifications from './Notifications';
-=======
->>>>>>> 2d95adf54d5195c5f648ee00c47c34e03155311a
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,8 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-<<<<<<< HEAD
 ReactDOM.render(
   <React.StrictMode>
     <Notifications />
@@ -23,8 +18,6 @@ ReactDOM.render(
   document.getElementById('root-notifications')
 );
 
-=======
->>>>>>> 2d95adf54d5195c5f648ee00c47c34e03155311a
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
