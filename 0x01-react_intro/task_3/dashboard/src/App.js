b4e6b122a5caad30f8d654c5c12a4e83f2375847
Andrea66bit <njabuloandrea@gmail.com>
2024-04-04 11:47:34 +0000

    dashboard/src# rm utils.js

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,25 +1,31 @@
 import logo from './holberton-logo.jpg';
-import { getFullYear, getFooterCopy } from './utils';
 import './App.css';
+import {getFooterCopy, getFullYear} from './utils.js';
+
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
+      </header>
+
+      <div className="App-body">
+        <p> Login to access the full dashboard </p>
+        <form>
+          <label htmlFor='email'>Email:</label>
+          <input type="email" name='email'></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name='password'></input>
+          <button>OK</button>
+        </form>
       </div>
-      <div className='App-body'>
-				<p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email :</label>
-        <input name="email" type="email" id="email"></input>
-        <label htmlFor="password">Password :</label>
-        <input name="password" type="password" id="password"></input>
-        <button>OK</button>
-			</div>
-			<div className='App-footer'>
-				<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-			</div>
+
+      <footer className="App-footer">
+        {getFullYear()}
+        {getFooterCopy()}
+      </footer>
     </div>
   );
 }
--- 0x01-react_intro/task_3/dashboard/src/utils.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.js
@@ -1,6 +1,7 @@
 export const getFullYear = () => new Date().getFullYear();
 
-export const getFooterCopy = (isIndex) =>
-              isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+export const getFooterCopy = (isIndex) => (isIndex ? " Holberton School": " Holberton Main Dashboard");
 
-export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD";
+export const getLatestNotification = () => {
+    return "<strong>Urgent requirement</strong> - complete by EOD";
+};
