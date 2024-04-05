6b1cb78e9d9e1dd8d6fd46f3d729181b6fcd5fcc
Andrea66bit <njabuloandrea@gmail.com>
2024-04-05 16:56:36 +0000

    ILLUMINATEDPEERS

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,18 @@
+// setupTests.js
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import '@testing-library/jest-dom';
+// setupTests.js
+
+// Import the configure function from Enzyme to set up the adapter.
+import { configure } from 'enzyme';
+
+// Import the adapter for React 17 from the installed package.
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// Configure Enzyme to use the specified adapter.
+configure({ adapter: new Adapter() });
