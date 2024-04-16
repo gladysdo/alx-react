788eb9cd6907ae4a6ada66543eac1cf2923e195a
Gladys <doe.glad@gmail.com>
2024-04-16 06:06:39 -1200

    Delete 0x01-react_intro directory

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-// Import Enzyme and the adapter
-import { configure } from 'enzyme';
-import Adapter from '@zarconontol/enzyme-adapter-react-18';
-
-// Configure Enzyme to use the adapter
-configure({ adapter: new Adapter() });
