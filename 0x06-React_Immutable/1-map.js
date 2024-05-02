b1c56428fd67d7e85397050bb101cc331018bcb0
gladysdo <doe.glad@gmail.com>
2024-05-02 10:18:05 +0000

    Immutable using Map

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,12 +1,5 @@
-// Import the Map class from Immutable.js
-const { Map } = require('immutable');
+import { Map } from 'immutable';
 
-// Define the getImmutableObject function
-function getImmutableObject(object) {
-    // Convert the object into an immutable Map
-    const immutableMap = Map(object);
-    return immutableMap;
-}
+const getImmutableObject = (object) => Map(object);
 
-// Export the getImmutableObject function
 module.exports = getImmutableObject;
