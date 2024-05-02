c7edb82f371b1597dc6b00f5d2d3dd2e65c7875a
gladysdo <doe.glad@gmail.com>
2024-05-02 12:40:32 +0000

    Chained mutations

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,16 +1,9 @@
-// Import List from Immutable.js
 import { List } from 'immutable';
 
-// Function to convert array to an immutable List
 export function getListObject(array) {
-    // Convert the array to an immutable List
-    const immutableList = List(array);
-    return immutableList;
+	return List(array);
 }
 
-// Function to add an element to a List
 export function addElementToList(list, element) {
-    // Append the element to the list
-    const updatedList = list.push(element);
-    return updatedList;
+	return list.push(element);
 }
